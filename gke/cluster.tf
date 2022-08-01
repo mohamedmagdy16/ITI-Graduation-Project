6287d348106e13d046821b9c75867fa9f3435645
Mohamed <mohamedmagdy16@github.com>
2022-08-01 08:38:01 +0200

    4th

--- gke/cluster.tf
+++ gke/cluster.tf
@@ -3,23 +3,65 @@ resource "google_container_cluster" "app_cluster" {
   project  = var.project_id
   name     = "gke-cluster"
   location = "us-central1-a"
+ node_locations = [ 
+        "us-central1-b"
 
-  ip_allocation_policy {
-
-  }
-    
- private_cluster_config {
-    enable_private_endpoint =false
-    enable_private_nodes    = true
-    master_ipv4_cidr_block  = "172.16.1.0/28"
-  }
-
+   ]
+#remove default node
   remove_default_node_pool = true
-  initial_node_count = 1
-
-   depends_on = [
+  
+#no of nodes in cluster
+  initial_node_count       = 1
+  depends_on = [
     var.network_name
  ]
+
+#range ips of pods,, range ips of service
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block = var.pods_ipv4_cidr_block
+    services_ipv4_cidr_block = var.services_ipv4_cidr_block
+  }
+  network = var.network_name
+  subnetwork = var.subnet_name
+
+  logging_service = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+  
+# enable cluster is private
+  private_cluster_config {
+    enable_private_endpoint = true
+    enable_private_nodes    = true
+    master_ipv4_cidr_block  = var.master_ipv4_cidr_block
+  }
+#update is stable
+  release_channel {
+      channel = "STABLE"
+  }
+
+  ###########access (authorize) vm to gke
+    master_authorized_networks_config {
+    cidr_blocks {
+        cidr_block   = var.master_authorized_networks_config
+        display_name = "External Control Plane access"
+      }
+  }
+
+  addons_config {
+    // Enable network policy (Calico)
+    network_policy_config {
+        disabled = false
+      }
+  }
+
+  /* Enable network policy configurations (like Calico).
+  For some reason this has to be in here twice. */
+  network_policy {
+    enabled = "true"
+  }
+#
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
 }
 
 # Small Linux node pool to run some Linux-only Kubernetes Pods.
--- network/nat.tf
+++ network/nat.tf
@@ -1,36 +1,30 @@
 resource "google_compute_router" "router" {
   name    = "router"
-  region  = var.region
   network = google_compute_network.vpc_network.id
 }
 
-resource "google_compute_address" "ip_nat" {
-  name         = "ip-nat"
-  address_type = "EXTERNAL"
-  network_tier = "PREMIUM"
+resource "google_compute_address" "ipnat" {
+  name         = "ipnat"
+  project = var.project_id
+  region = var.region
 
 }
 
 resource "google_compute_router_nat" "nat" {
   name   = "nat"
   router = google_compute_router.router.name
-  region = var.region
-
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  region = google_compute_router.router.region
   nat_ip_allocate_option             = "MANUAL_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ips = [ google_compute_address.ipnat.self_link ]
 
   subnetwork {
     name                    = google_compute_subnetwork.Management_Subnet.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+    source_ip_ranges_to_nat = [var.management_subnet_cidr]
   }
 
-
-  subnetwork {
-    name                    = google_compute_subnetwork.Restricted_Subnet.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  subnetwork { 
+     name = google_compute_subnetwork.Restricted_Subnet.id
+     source_ip_ranges_to_nat = [ var.restricted_subnet_cidr] # "ALL_IP_RANGES"
   }
-
-  
-  nat_ips = [google_compute_address.ip_nat.self_link]
 }
-
